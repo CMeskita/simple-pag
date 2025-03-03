1b2621bd2e98f159c66cb2059a7858f21ea9ab78
Deassis Barbosa <diassisbarbosa.db@gmail.com>
2025-03-03 15:26:48 -0300

    Classe Forma Pagamento e Usuário

---
+++ simple-pag-Domain/Entity/FormaPagamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace simple_pag_Domain.Entity
+{
+    public class FormaPagamento
+    {
+        public string Id { get; protected set; }
+        public string Nome { get; protected set; }
+        public int CodFinalizadora { get; protected set; }
+        public DateTime Registro { get; protected set; }
+        public string Sigla { get; protected set; }
+        public bool Status { get; protected set; }
+
+
+        public FormaPagamento(string nome, int codFinalizadora, string sigla)
+        {
+            Id = Guid.NewGuid().ToString().ToUpper();
+            Nome = nome;
+            CodFinalizadora = codFinalizadora;
+            Registro = DateTime.UtcNow;
+            Sigla = sigla;
+            Status = true;
+        }
+    }
+}
---
+++ simple-pag-Domain/Entity/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_pag_Domain.Entity
+{
+    public class Usuario
+    {
+        public Usuario(string nome, string email, string chavePrivada)
+        {
+            Id = Guid.NewGuid().ToString().ToUpper();
+            Nome = nome;
+            Email = email;
+            ChavePrivada = chavePrivada;
+            Registro = DateTime.UtcNow;
+            Status = true;
+        }
+
+        public string Id { get; protected set; }
+        public string Nome { get; protected set; }
+        public string Email { get; protected set; }
+        public string ChavePrivada { get; protected set; }
+        public DateTime Registro { get; protected set; }
+        public bool Status { get; protected set; }
+
+    }
+}
