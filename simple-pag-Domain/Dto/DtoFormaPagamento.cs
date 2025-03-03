c77bd57acf0cf1b42c84777b0461b4cfa5c97088
Deassis Barbosa <diassisbarbosa.db@gmail.com>
2025-03-03 18:09:07 -0300

    Criado o DtoFormaPagamento
    Criado o uma pasta Dto para criar a classe Dto Forma de pagamento.

---
+++ simple-pag-Domain/Dto/DtoFormaPagamento.cs
@@ -0,0 +1,13 @@
+﻿using simple_pag_Domain.Entity;
+namespace simple_pag_Domain.Dto
+{
+    public class DtoFormaPagamento
+    {
+        public string Nome { get; set; }
+        public int CodFinalizadora {  get; set; }
+        public string Sigla {  get; set; }
+
+        public static implicit operator FormaPagamento(DtoFormaPagamento formpag)
+            => new FormaPagamento(formpag.Nome, formpag.CodFinalizadora, formpag.Sigla);
+    }
+}
