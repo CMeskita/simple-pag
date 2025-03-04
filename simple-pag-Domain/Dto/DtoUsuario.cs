51e83db225e5a49cf800020808f6b7c67a855da1
Deassis Barbosa <diassisbarbosa.db@gmail.com>
2025-03-04 09:36:01 -0300

    Criação do DtoUsuario
    Foi criado classe DtoUsuario.cs

---
+++ simple-pag-Domain/Dto/DtoUsuario.cs
@@ -0,0 +1,15 @@
+﻿
+using simple_pag_Domain.Entity;
+
+namespace simple_pag_Domain.Dto
+{
+    public class DtoUsuario
+    {
+        public string Nome {  get; set; }
+        public string Email { get; set; }
+        public string ChavePrivada { get; set; }
+
+        public static implicit operator Usuario(DtoUsuario usuario)
+            => new Usuario(usuario.Nome, usuario.Email, usuario.ChavePrivada);
+    }
+}
