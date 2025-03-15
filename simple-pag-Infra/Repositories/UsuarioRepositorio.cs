15a210dd9bed1e2cae60df93c57eb8abc4ce56d0
Danielle Mesquita <danielle.cmesquita@gmail.com>
2025-03-15 20:34:59 -0300

    Repositorio Usuario implementado

--- simple-pag-Infra/Repositories/UsuarioRepositorio.cs
+++ simple-pag-Infra/Repositories/UsuarioRepositorio.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using simple_pag_Domain.Entity;
+using simple_pag_Domain.Interface;
+using simple_pag_Infra.Conection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,49 @@ using System.Threading.Tasks;
 
 namespace simple_pag_Infra.Repositories
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio : IUsuarioRepositorio
     {
+        private readonly Context _context;
 
+        public UsuarioRepositorio(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUsuario(Usuario usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            _context.SaveChanges();
+
+        }
+
+        public bool ExisteUsuario(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Usuario?> FindUsuarioById(string id)
+        {
+            var result = await _context.Usuarios.FindAsync(id);
+            return result;
+        }
+
+        public async Task<IList<Usuario>> GetAllUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        public Task InativarUsuario(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Usuario dados)
+        {
+            _context.Usuarios.Update(dados);
+            await _context.SaveChangesAsync();
+        }
+
+      
     }
 }
