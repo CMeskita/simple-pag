3a573e83fb19249b0cbaa68de1dcf9ede028460c
Danielle Mesquita <danielle.cmesquita@gmail.com>
2025-03-17 16:44:05 -0300

    MongoDB

---
+++ simple-pag-Domain/Models/LogInformation.cs
@@ -0,0 +1,17 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace simple_pag_Domain.Models
+{
+    public class LogInformation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Classe { get; set; }
+        public string? Informacao { get; set; }
+        public string? registro { get; set; }=DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+    }
+}
